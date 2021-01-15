3e875ec74f9d0d6b807033ab654ac390fa4a0993
Vault108 <44564111+Vault108@users.noreply.github.com>
2021-01-14 20:01:57 -0500

    code cleanup
    Signed-off-by: Vault108 <44564111+Vault108@users.noreply.github.com>

--- PInfo.py
+++ PInfo.py
@@ -7,6 +7,7 @@ from rich.panel import Panel
 from rich.console import Console
 from rich import print
 from rich.console import render_group
+mine = '0.3.0'
 
 
 def hinfo():
@@ -14,40 +15,38 @@ def hinfo():
 
 
 def dinfo():  # Defines the dns command
-    console = Console()
-    url = console.input(
-        "[blue]Enter a Domain[/blue] :warning:[bold red](Do not use WWW)[/bold red]:warning: [blue][/blue]"
+    url = Console().input(
+        "[blue]Enter a Domain[/blue] :warning:[bold red1](Do not use WWW)[/bold red1]:warning: [blue]"
     )
     for a in dns.resolver.resolve(url, "a"):
-        print(
-            "[red]  A Record :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red]",
+       Console().print(
+            "[red3]  A Record :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/]",
             a,
         )
     for mx in dns.resolver.resolve(url, "mx"):
-        print(
-            "[red] MX Record :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red]",
+        Console().print(
+            "[red3] MX Record :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/]",
             mx,
         )
     for ns in dns.resolver.resolve(url, "ns"):
-        print(
-            "[red] NS Record :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red]",
+       Console().print(
+            "[red3] NS Record :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/]",
             ns,
         )
 
 
 def ipi():  # Defines ip info command
-    console = Console()
     keys_file = open("keys.txt")
     read = keys_file.readlines()
     access_token = read[0].rstrip()
     handler = ipinfo.getHandler(access_token)
-    ip_address = console.input("[bold blue]Enter an IP: [/bold blue]")
+    ip_address = Console().input("[bold blue]Enter an IP: [/bold blue]")
     details = handler.getDetails(ip_address)
     ip = details.ip
     city = details.city
     region = details.region
     country = details.country_name
-    ping = subprocess.check_output("ping -c 4 " + ip_address, shell=True)
+    ping = subprocess.check_output("ping -c 4 " + ip_address + "| tail -n 1", shell=True)
     ping_out = ping.decode()
     try:
         hn = details.hostname
@@ -55,40 +54,31 @@ def ipi():  # Defines ip info command
     except AttributeError:
         hn = "No Hostname found for requested IP "
         org = "No Organization found for requested IP"
-
-    @render_group()
-    def get_panels():
-        yield Panel(
-            city + "," + region + " " + country,
-            style="black on spring_green2",
-            title="Location",
-        )
-        yield Panel(org, style="black on spring_green1", title="Organization")
-        yield Panel(hn, style="black on medium_spring_green", title="Hostnmame")
-        yield Panel(ping_out, style="black on cyan2", title="Ping")
-
-    rich.print(Panel(get_panels(), title="INFO FOR " + ip))
-
+    Console().print("[red3] INFO FOR [/red3]", ip)
+    Console().print("[red3] Location :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red3] ", city + "," + region + country )
+    Console().print("[red3] Hostname :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red3] ", hn )
+    Console().print("[red3] Organization :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red3] ", org)
+    Console().print("[red3] Ping :arrow_forward:  :arrow_forward:  :arrow_forward:  :arrow_forward: [/red3] ", ping_out, )
 
 def main():
 
 
     while True:
-        mine = '0.3.0'
         Version = requests.get('https://vault108.github.io/PInfo/about.json').json()["Version"]
         rdate = requests.get('https://vault108.github.io/PInfo/about.json').json()["Date"]
-        if Version == mine: print("You have the latest version")
+        if Version == mine: Console().print("[cyan1] \n  You have the latest version [/cyan1]")
         if Version > mine: print("Version", mine + " is outdated. Version " + Version + " was released " + rdate )
         if Version < mine: print("Do you have a time machine?")
-        print("\nChoose service you want to use : ")
-        print(
+        print("\n[royal_blue1]Choose service you want to use : [/royal_blue1] ")
+        print("[royal_blue1]"
             """
         1 : Check an IP
         2 : Check a URL
         3 : Check DNS
         0 : Exit"""
+	"[/royal_blue1]"
         )
-        choice = input("\nEnter your choice : ")
+        choice = Console().input("\n[royal_blue1]Enter your choice : [/royal_blue1]")
 
         if choice == "1":
             ipi()
